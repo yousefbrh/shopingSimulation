28ebbc1fb8bc763bfe1caa7bd900f977ca5b7f46
Yousef <yousefborhanii@gmail.com>
2023-05-09 13:56:42 +0330

    Implement animation changing base on input logic

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,13 +7,18 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float speed = 5f;
+    [SerializeField] private Animator animator;
     private Vector2 _inputMovement;
+    private static readonly int X = Animator.StringToHash("X");
+    private static readonly int Y = Animator.StringToHash("Y");
 
     private void FixedUpdate()
     {
         _inputMovement.x = Input.GetAxis("Horizontal");
         _inputMovement.y = Input.GetAxis("Vertical");
-        
+        animator.SetFloat(X, _inputMovement.x);
+        animator.SetFloat(Y, _inputMovement.y);
+
         rb.MovePosition(rb.position + speed * _inputMovement * Time.fixedDeltaTime);
     }
 }
